a76be36091ade0337a89fd140cf62f29b90b18a4
Kumar Vavilala <s.kumar.vavilala@accenture.com>
2019-06-20 17:40:51 +0530

    changes to package json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { EzNumericboxModule,  EzNumericboxService } from 'ez-numericbox'
 import { AppComponent } from './app.component';
-import { NumericBoxComponent } from './numeric-box/numeric-box.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NumericBoxComponent,
   ],
   imports: [
     BrowserModule,
